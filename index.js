578859c8533ffc13e40f1552354f7684cc4f6e44
Habsa Abdirizack <habsa.abdirizack@student.moringaschool.com>
2024-09-28 14:29:30 +0300

    assignments

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+
+// Returns First two drivers
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0,2);
+}
+
+// This returns the last two drivers
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(-2);
+
+}
+
+const selectingDrivers  =[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+    return function(fare){
+        return fare * multiplier
+    }
+}
+function fareDoubler (fare){
+    return fare * 2
+}
+
+const fareTripler = createFareMultiplier (3);
+function selectDifferentDrivers(drivers,fn){
+    return fn(drivers);
+}
